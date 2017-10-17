11bcc12f3471662ecac731e04cc99a724c5cea23
tiemonl <tiemon.liam@gmail.com>
2017-10-17 16:38:16 -0400

    added a navigation bar and made it switch between activities

--- app/src/main/java/com/tylerdarby/charactersheet/activities/CharacterCreation.java
+++ app/src/main/java/com/tylerdarby/charactersheet/activities/CharacterCreation.java
@@ -1,9 +1,18 @@
 package com.tylerdarby.charactersheet.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.tylerdarby.charactersheet.R;
+
+import java.lang.reflect.Field;
 
 public class CharacterCreation extends AppCompatActivity {
 
@@ -11,5 +20,60 @@ public class CharacterCreation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_creation);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+        MainActivity.BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_new_char:
+                                Intent charCreation = new Intent(getApplicationContext(), CharacterCreation.class);
+                                startActivity(charCreation);
+                                break;
+                            case R.id.action_dice:
+                                Intent diceRoller = new Intent(getApplicationContext(), DiceRoller.class);
+                                startActivity(diceRoller);
+                                break;
+                            case R.id.action_home:
+                                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(mainActivity);
+                                break;
+                            case R.id.action_new_user:
+                                Intent userReg = new Intent(getApplicationContext(), UserRegistration.class);
+                                startActivity(userReg);
+                                break;
+                            case R.id.action_reserved:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+    }
+    public static class BottomNavigationViewHelper {
+        public static void disableShiftMode(BottomNavigationView view) {
+            BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+            try {
+                Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+                shiftingMode.setAccessible(true);
+                shiftingMode.setBoolean(menuView, false);
+                shiftingMode.setAccessible(false);
+                for (int i = 0; i < menuView.getChildCount(); i++) {
+                    BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                    //noinspection RestrictedApi
+                    item.setShiftingMode(false);
+                    // set once again checked value, so view will be updated
+                    //noinspection RestrictedApi
+                    item.setChecked(item.getItemData().isChecked());
+                }
+            } catch (NoSuchFieldException e) {
+                Log.e("BNVHelper", "Unable to get shift mode field", e);
+            } catch (IllegalAccessException e) {
+                Log.e("BNVHelper", "Unable to change value of shift mode", e);
+            }
+        }
     }
 }
